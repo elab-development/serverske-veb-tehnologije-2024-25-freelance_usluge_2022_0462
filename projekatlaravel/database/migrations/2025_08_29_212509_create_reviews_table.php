b204376e1813dfcc5bb3ab8608848dffcb85efab
AntoninaProticc <antprotic@gmail.com>
2025-08-29 23:27:31 +0200

    dodate migracije za kreiranje tabela

---
+++ projekatlaravel/database/migrations/2025_08_29_212509_create_reviews_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+           $table->id();
+            $table->foreignId('project_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('reviewer_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('reviewee_id')->constrained('users')->cascadeOnDelete();
+            $table->unsignedTinyInteger('rating'); // 1–5
+            $table->text('comment')->nullable();
+            $table->timestamps();
+
+            $table->index(['reviewee_id', 'rating']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
